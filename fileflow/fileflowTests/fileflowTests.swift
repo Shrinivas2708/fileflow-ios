935f8b54d7c60b94a3e31b684c0163eeefdb290b
Shri <76138254+Shrinivas2708@users.noreply.github.com>
2024-11-21 14:19:23 +0530

    Add files via upload

---
+++ fileflow/fileflowTests/fileflowTests.swift
@@ -0,0 +1,17 @@
+//
+//  fileflowTests.swift
+//  fileflowTests
+//
+//  Created by omjadhav on 17/11/24.
+//
+
+import Testing
+@testable import fileflow
+
+struct fileflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
